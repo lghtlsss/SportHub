d2125a6fb06eaa22ddadcb83bf4a5eddcba124af
1lght <guryanovartem2008@gmail.com>
2026-04-27 16:09:58 +0300

    empty routes

--- main_app/app.py
+++ main_app/app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from flask_login import login_required, logout_user, current_user
 
 app = Flask(__name__)
 
@@ -7,6 +8,16 @@ app = Flask(__name__)
 @app.route("/index")
 def index():
     return render_template("index.html", title='Главная')
+
+
+@app.route('/profile')
+def profile():
+    return
+
+
+@app.route('/subscriptions')
+def subscriptions():
+    return
 
 
 def main():
