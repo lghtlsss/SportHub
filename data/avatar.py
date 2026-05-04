748bf3f9e496e96c8206297eb097417a0dd2c3fd
1lght <guryanovartem2008@gmail.com>
2026-05-04 14:22:38 +0300

    extends previous commit

---
+++ data/avatar.py
@@ -0,0 +1,11 @@
+from sqlalchemy import Column, Integer, LargeBinary, String, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy_serializer import SerializerMixin
+from .db_session import SqlAlchemyBase
+
+
+class Avatar(SqlAlchemyBase, SerializerMixin):
+    __tablename__ = 'Avatars'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    content = Column(LargeBinary)
+    user_id = Column(Integer, ForeignKey('Users.id'))
