f1bbd444cb88f662697721a5b07bb8cdc1b5cb41
1lght <guryanovartem2008@gmail.com>
2026-05-04 14:40:30 +0300

    registration form

--- main_app/forms/registration_form.py
+++ main_app/forms/registration_form.py
@@ -1,16 +1,19 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, IntegerField, EmailField, SelectMultipleField, SubmitField
+from wtforms import PasswordField, StringField, IntegerField, EmailField, SelectMultipleField, SubmitField, FileField
 from wtforms.validators import DataRequired
+from flask_wtf.file import FileAllowed
 
 
 class RegisterForm(FlaskForm):
     email = EmailField('Введите вашу почту', validators=[DataRequired()])
+    avatar = FileField('Прикрепите аватар(По желанию)',
+                       validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Только изображения (.jpg, .png, .jpeg)!')])
     name = StringField('Введите ваше имя', validators=[DataRequired()])
     surname = StringField('Введите вашу фамилию', validators=[DataRequired()])
     age = IntegerField('Введите ваш возраст', validators=[DataRequired()])
     password = PasswordField('Придумайте пароль', validators=[DataRequired()])
     password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
-    description = StringField('О себе(по желанию)')
+    description = StringField('О себе(По желанию)')
     pref_sport = SelectMultipleField('Выберите ваш спорт', validators=[DataRequired()],
                                      choices=[(1, 'Бег'), (2, 'Велоспорт'), (3, 'Тяжелая атлетика'),
                                               (4, 'Фитнес/Похудение')])
