8b6af670fc4897aa2a0a124bff71a63c957c93c7
1lght <guryanovartem2008@gmail.com>
2026-05-04 21:09:30 +0300

    post creation fix

--- main_app/forms/post_creation_form.py
+++ main_app/forms/post_creation_form.py
@@ -1,11 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms.validators import DataRequired
-from wtforms import SubmitField, StringField
+from wtforms import SubmitField, StringField, FileField, TextAreaField
+from flask_wtf.file import FileAllowed
 
 
 class PostCreation(FlaskForm):
     title = StringField('Заголовок поста', validators=[DataRequired()])
-    text = StringField('Текст поста', validators=[DataRequired()])
-    contents = StringField('Вложения **пока не работает**')
-    topic = StringField('Тема(не обязательно)')
+    text = TextAreaField('Текст поста', validators=[DataRequired()])
+    contents = FileField('Прикрепите фотографию',
+                         validators=[FileAllowed(['jpg', 'png', 'jpeg'], 'Только изображения (.jpg, .png, .jpeg)!')])
+    topic = StringField('Вид спорта(не обязательно)')
     submit = SubmitField('Опубликовать')
