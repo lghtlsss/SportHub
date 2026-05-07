5d37cc4f510869e03abe93e08c2e5ccdcbdc1c26
1lght <guryanovartem2008@gmail.com>
2026-05-07 21:25:09 +0300

    Fix: avatar resource

--- resources/avatar_resource.py
+++ resources/avatar_resource.py
@@ -1,20 +1,21 @@
 from flask import jsonify, Response
 from flask_restful import Resource, abort
 
-from data.avatar import Avatar
+from data import avatar
+from data.__all_models import Avatar, User
 from data.db_session import create_session
 
 
-def abort_if_not_found(session, thing_id):
-    thing = session.get(Avatar, thing_id)
+def abort_if_not_found(session, thing_id, thing_class):
+    thing = session.get(thing_class, thing_id)
     if not thing:
         abort(404, message='Not found')
 
 
 class AvatarResource(Resource):
-    def get(self, avatar_id):
+    def get(self, user_id):
         session = create_session()
-        abort_if_not_found(session, avatar_id)
-        avatar = session.get(Avatar, avatar_id)
+        abort_if_not_found(session, user_id, User)
+        user = session.get(User, user_id)
+        avatar = user.avatar
         return Response(avatar.content, mimetype=avatar.mime)
-
