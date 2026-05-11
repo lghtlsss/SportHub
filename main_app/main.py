bb81b529e2b9c9cf121fd8e12b23024bc6f49c3e
1lght <guryanovartem2008@gmail.com>
2026-05-11 17:11:57 +0300

    port fix(почему-то не встаёт на 8080, сделал 8070)

--- main_app/main.py
+++ main_app/main.py
@@ -17,7 +17,7 @@ from PIL import Image as Im
 import io
 from tools import time_tool, image_request_tool
 
-# TODO: пофиксить проблему с падением сайта из за большого кол-ва запросов
+# TODO: пофиксить проблему с падением сайта из-за большого кол-ва запросов
 # TODO: flask-limiter
 
 login_manager = LoginManager()
--- main_app/tools/image_request_tool.py
+++ main_app/tools/image_request_tool.py
@@ -2,7 +2,7 @@ from requests import get
 
 
 def check_image(post_id):
-    response = get(f'http://localhost:8080/api/images/{post_id}')
+    response = get(f'http://localhost:8070/api/images/{post_id}')
     if response.content:
         return True
     return False
