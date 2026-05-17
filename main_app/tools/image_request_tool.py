dcfd535209bf1ddb173d335ee238a12c4e926029
1lght <guryanovartem2008@gmail.com>
2026-05-17 17:43:06 +0300

    Mini-description for functions

--- main_app/tools/image_request_tool.py
+++ main_app/tools/image_request_tool.py
@@ -1,7 +1,8 @@
 from requests import get
 
 
-def check_image(post_id):
+def check_image(post_id) -> bool:
+    """Проверяет, есть ли картинка по этому адресу"""
     response = get(f'http://localhost:8070/api/images/{post_id}')
     if response.content:
         return True
